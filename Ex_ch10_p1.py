70d8fb71ebc24548c2f99045d07e4359b4cf7829
hanseokim728 <hanseokim728@snu.ac.kr>
2025-11-23 09:47:18 +0900

    Add files via upload

---
+++ Ex_ch10_p1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("나의 소개 페이지")
+
+st.header("자기소개")
+st.write("안녕하세요, 저는 응용생물화학부 김한서입니다.")
+st.write("저는 생명공학과 _인공지능_에 관심이 있습니다.")
+
+st.header("좋아하는 것")
+st.write("저는 여행과 요리를 좋아합니다.")
+st.write("가장 자주 방문하는 사이트는 [ChatGPT](https://chatgpt.com/) 입니다.")
+
+st.header("앞으로의 목표")
+st.write("앞으로 다양한 프로그램을 만들어보면서 사람들에게 정말 도움이 될 쉽고 효율적인 앱을 만들어보고 싶습니다.")
